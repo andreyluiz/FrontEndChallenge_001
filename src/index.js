2b645befcd9851709a5d17e3f6849b2d2be23a3c
Andrey Luiz <andreyluiz.developer@gmail.com>
2016-11-19 10:15:20 -0200

    Create store with the reducer.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import rootReducer from './reducers';
+
 import App from './App';
 import './index.css';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
   <App />,
