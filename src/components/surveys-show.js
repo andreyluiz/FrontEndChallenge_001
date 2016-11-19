ff14f735d0922362eae4cfbd05537243496f3d4d
Andrey Luiz <andreyluiz.developer@gmail.com>
2016-11-19 16:39:28 -0200

    Create some styling for the single survey view.

--- src/components/surveys-show.js
+++ src/components/surveys-show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { fetchSurvey } from '../actions';
+import './surveys-show.css';
 
 class SurveysShow extends Component {
   componentWillMount() {
@@ -14,27 +15,40 @@ class SurveysShow extends Component {
       return (<p>Loading...</p>);
     }
 
+    let questionCount = 1;
+
     return (
-      <div>
-        <h3>{survey.title}</h3>
-        <p>{survey.tagline}</p>
+      <div className="survey-show">
+        <h1>{survey.title}</h1>
+        <h3>{survey.tagline}</h3>
 
         <form>
-          {
-            survey.questions.map(question => (
-              <div>
-                <p>{question.title}</p>
-                {
-                  question.options.map(option => (
-                    <div>
+          {survey.questions.map(question => (
+            <div className="survey-question">
+              <p><b>{questionCount++})</b> {question.title}</p>
+              <div className="question-options">
+                {question.options.map(option => (
+                  <div className="option">
+                    <label>
                       <input type="radio" value={option}/>
-                      <label>{option}</label>
-                    </div>
-                  ))
-                }
+                      <span>{option}</span>
+                    </label>
+                  </div>
+                ))}
               </div>
-            ))
-          }
+            </div>
+          ))}
+          <div className="survey-controls">
+            <div className="button-container">
+              <div className="button-relative">
+                <button type="submit">Submit</button>
+              </div>
+            </div>
+            <div className="link-container">
+              <Link to="/">Cancel</Link>
+            </div>
+            <div className="controls-spacer"></div>
+          </div>
         </form>
       </div>
     );
